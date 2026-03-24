6743d2e6a37fceb43858c9ba51a48e576bd8aad9
ST10445680-Thembelihle-Mhlanga <ST10445680@MYEMERIS.EDU.ZA>
2026-03-24 10:56:47 +0200

    Thembelihleisbeautiful

--- obj/Debug/net10.0/practicepush.AssemblyInfo.cs
+++ obj/Debug/net10.0/practicepush.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("practicepush")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+005a3d309aed6f48c0fa416d072d0fd40a6083c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("practicepush")]
 [assembly: System.Reflection.AssemblyTitleAttribute("practicepush")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

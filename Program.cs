005a3d309aed6f48c0fa416d072d0fd40a6083c9
ST10445680-Thembelihle-Mhlanga <ST10445680@MYEMERIS.EDU.ZA>
2026-03-24 10:43:55 +0200

    Hello Thembelihle

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace practicepush
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Thembelihle!");
+        }
+    }
+}
